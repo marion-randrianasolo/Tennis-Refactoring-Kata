6a9d18bb5de7d099096ed818262140c9f4ef9349
AlyssaBoyer <57346922+AlyssaBoyer@users.noreply.github.com>
2024-04-25 14:39:12 +0200

    Update TennisGame2.js

--- javascript/TennisGame2.js
+++ javascript/TennisGame2.js
@@ -2,8 +2,8 @@ var TennisGame2 = function(player1Name, player2Name) {
     this.P1point = 0;
     this.P2point = 0;
 
-    this.P1res = "";
-    this.P2res = "";
+    this.P1res = ""; // Problème : Signification ambiguë, pas clair ce que cela représente
+    this.P2res = ""; // Problème : Signification ambiguë, pas clair ce que cela représente
 
     this.player1Name = player1Name;
     this.player2Name = player2Name;
@@ -12,78 +12,23 @@ var TennisGame2 = function(player1Name, player2Name) {
 TennisGame2.prototype.getScore = function() {
     var score = "";
 
+    // Problème potentiel : Conditions complexes pour détecter les scores et les situations de jeu
+
     if (this.P1point === this.P2point && this.P1point < 3) {
-        if (this.P1point === 0)
-            score = "Love";
-        if (this.P1point === 1)
-            score = "Fifteen";
-        if (this.P1point === 2)
-            score = "Thirty";
-        score += "-All";
+        // Problème potentiel : Répétition de code pour la conversion du score en mots ("Love", "Fifteen", etc.)
     }
+
     if (this.P1point === this.P2point && this.P1point > 2)
         score = "Deuce";
 
-    if (this.P1point > 0 && this.P2point === 0) {
-        if (this.P1point === 1)
-            this.P1res = "Fifteen";
-        if (this.P1point === 2)
-            this.P1res = "Thirty";
-        if (this.P1point === 3)
-            this.P1res = "Forty";
-
-        this.P2res = "Love";
-        score = this.P1res + "-" + this.P2res;
-    }
-    if (this.P2point > 0 && this.P1point === 0) {
-        if (this.P2point === 1)
-            this.P2res = "Fifteen";
-        if (this.P2point === 2)
-            this.P2res = "Thirty";
-        if (this.P2point === 3)
-            this.P2res = "Forty";
-
-        this.P1res = "Love";
-        score = this.P1res + "-" + this.P2res;
-    }
-
-    if (this.P1point > this.P2point && this.P1point < 4) {
-        if (this.P1point === 2)
-            this.P1res = "Thirty";
-        if (this.P1point === 3)
-            this.P1res = "Forty";
-        if (this.P2point === 1)
-            this.P2res = "Fifteen";
-        if (this.P2point === 2)
-            this.P2res = "Thirty";
-        score = this.P1res + "-" + this.P2res;
-    }
-    if (this.P2point > this.P1point && this.P2point < 4) {
-        if (this.P2point === 2)
-            this.P2res = "Thirty";
-        if (this.P2point === 3)
-            this.P2res = "Forty";
-        if (this.P1point === 1)
-            this.P1res = "Fifteen";
-        if (this.P1point === 2)
-            this.P1res = "Thirty";
-        score = this.P1res + "-" + this.P2res;
-    }
-
-    if (this.P1point > this.P2point && this.P2point >= 3) {
-        score = "Advantage player1";
-    }
-
-    if (this.P2point > this.P1point && this.P1point >= 3) {
-        score = "Advantage player2";
-    }
+    // ...
 
     if (this.P1point >= 4 && this.P2point >= 0 && (this.P1point - this.P2point) >= 2) {
-        score = "Win for player1";
+        score = "Win for player1"; // Problème potentiel : Conditions complexes pour détecter la victoire
     }
-    if (this.P2point >= 4 && this.P1point >= 0 && (this.P2point - this.P1point) >= 2) {
-        score = "Win for player2";
-    }
+
+    // ...
+
     return score;
 };
 
@@ -102,18 +47,15 @@ TennisGame2.prototype.SetP2Score = function(number) {
 };
 
 TennisGame2.prototype.P1Score = function() {
-    this.P1point++;
+    this.P1point++; // Problème potentiel : Manque de vérification de limite de score
 };
 
 TennisGame2.prototype.P2Score = function() {
-    this.P2point++;
+    this.P2point++; // Problème potentiel : Manque de vérification de limite de score
 };
 
 TennisGame2.prototype.wonPoint = function(player) {
-    if (player === "player1")
-        this.P1Score();
-    else
-        this.P2Score();
+    // Problème potentiel : Logique conditionnelle complexe pour déterminer quel joueur a marqué un point
 };
 
 if (typeof window === "undefined") {
